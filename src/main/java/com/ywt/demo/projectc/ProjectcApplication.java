7f5987e3c69443d3a56b278b6a24c2469743e28f
NeverMore93 <1305882275@qq.com>
2018-05-13 21:00:07 +0800

    Initial commit

---
+++ src/main/java/com/ywt/demo/projectc/ProjectcApplication.java
@@ -0,0 +1,12 @@
+package com.ywt.demo.projectc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcApplication.class, args);
+	}
+}
